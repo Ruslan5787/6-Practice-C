c4308a738773bf635434b7b870d932c87850fb2b
AymanZhus <Zhusupova106@gmail.com>
2024-02-10 11:16:29 +0500

    m

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>;
+#include <string.h>
+using namespace std;
+
+bool glasnaya(char a) {
+	char glasnie[] = { 'a', 'i', 'o','y','e', 'u', 'A', 'I', 'O', 'Y', 'E', 'U'};
+	bool isGlasnie = false;
+
+	for (int i = 0; i < sizeof(glasnie); i++)
+	{
+		if (a == glasnie[i]) {
+			isGlasnie = true;
+		}
+	}
+
+	return isGlasnie;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	char string[255];
+	cout << "Введите слово - ";
+	gets_s(string);
+
+	for (int i = 0; i <= strlen(string); i++)
+	{
+		if (!glasnaya(string[i])) {
+			cout << string[i];
+		}
+	}
+}
